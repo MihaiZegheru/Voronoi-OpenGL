73d002c06af24a852c04e054dc488e5560857f2b
MihaiZ <mihai.z.2005@gmail.com>
2022-11-30 14:11:35 +0200

    Separated inputmanager.cpp from inputmanager.h
    Signed-off-by: MihaiZ <mihai.z.2005@gmail.com>

--- src/inputmanager.cpp
+++ src/inputmanager.cpp
@@ -1,1 +1,134 @@
 #include <inputmanager.h>
+
+InputManager& InputManager::GetInstance() {
+		static InputManager instance;
+		return instance;
+}
+
+void InputManager::Listen(GLFWwindow* window)
+{
+    // Vertical Axis
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        m_vertical = 1;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_RELEASE)
+    {
+        if (m_vertical > 0)
+        {
+            m_vertical = 0;
+        }
+    }
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        m_vertical = -1;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_W) == GLFW_RELEASE)
+    {
+        if (m_vertical < 0)
+        {
+            m_vertical = 0;
+        }
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS && glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        m_vertical = 0;
+    }
+
+    // Horizontal Axis
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        m_horizontal = 1;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_RELEASE)
+    {
+        if (m_horizontal > 0)
+        {
+            m_horizontal = 0;
+        }
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        m_horizontal = -1;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_RELEASE)
+    {
+        if (m_horizontal < 0)
+        {
+            m_horizontal = 0;
+        }
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS && glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        m_horizontal = 0;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+    {
+        m_jump = true;
+    }
+    else
+    {
+        m_jump = false;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+    {
+        m_leftshift = true;
+    }
+    else
+    {
+        m_leftshift = false;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+    {
+        m_Q = true;
+    }
+    else
+    {
+        m_Q = false;
+    }
+
+    double mouseX, mouseY;
+    glfwGetCursorPos(window, &mouseY, &mouseX);
+    m_mousePosition.x = mouseX;
+    m_mousePosition.y = mouseY;
+}
+
+float InputManager::GetAxis(std::string axis)
+{
+    if (axis == "horizontal")
+    {
+        return m_horizontal;
+    }
+    if (axis == "vertical")
+    {
+        return m_vertical;
+    }
+}
+
+bool InputManager::GetKey(std::string key)
+{
+    if (key == "jump")
+    {
+        return m_jump;
+    }
+    if (key == "leftshift")
+    {
+        return m_leftshift;
+    }
+    if (key == "pause")
+    {
+        return m_Q;
+    }
+}
+
+InputManager::InputManager() {
+    m_horizontal = 0;
+    m_vertical = 0;
+    m_jump = false;
+    m_leftshift = false;
+    m_Q = false;
+    m_mousePosition = glm::vec2(0, 0);
+}
--- src/inputmanager.h
+++ src/inputmanager.h
@@ -20,146 +20,16 @@
 class InputManager
 {
 public:
-	static InputManager& GetInstance()
-	{
-		static InputManager instance;
-		return instance;
-	}
+    static InputManager& GetInstance();
 
-	void Listen(GLFWwindow* window)
-	{
-        // Vertical Axis
-        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        {
-            m_vertical = 1;
-        }
-        else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_RELEASE)
-        {
-            if (m_vertical > 0)
-            {
-                m_vertical = 0;
-            }
-        }
-        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        {
-            m_vertical = -1;
-        }
-        else if (glfwGetKey(window, GLFW_KEY_W) == GLFW_RELEASE)
-        {
-            if (m_vertical < 0)
-            {
-                m_vertical = 0;
-            }
-        }
-        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS && glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        {
-            m_vertical = 0;
-        }
+    void Listen(GLFWwindow* window);
 
-        // Horizontal Axis
-        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        {
-            m_horizontal = 1;
-        }
-        else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_RELEASE)
-        {
-            if (m_horizontal > 0)
-            {
-                m_horizontal = 0;
-            }
-        }
-        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        {
-            m_horizontal = -1;
-        }
-        else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_RELEASE)
-        {
-            if (m_horizontal < 0)
-            {
-                m_horizontal = 0;
-            }
-        }
-        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS && glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        {
-            m_horizontal = 0;
-        }
-
-        if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
-        {
-            m_jump = true;
-        }
-        else
-        {
-            m_jump = false;
-        }
-
-        if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
-        {
-            m_leftshift = true;
-        }
-        else
-        {
-            m_leftshift = false;
-        }
-
-        if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
-        {
-            m_Q = true;
-        }
-        else
-        {
-            m_Q = false;
-        }
-
-        double mouseX, mouseY;
-        glfwGetCursorPos(window, &mouseY, &mouseX);
-        m_mousePosition.x = mouseX;
-        m_mousePosition.y = mouseY;
-	}
-
-    float GetAxis(std::string axis)
-    {
-        if (axis == "horizontal")
-        {
-            return m_horizontal;
-        }
-        if (axis == "vertical")
-        {
-            return m_vertical;
-        }
-    }
-
-    glm::vec2 GetMousePosition()
-    {
-        return m_mousePosition;
-    }
-
-    bool GetKey(std::string key)
-    {
-        if (key == "jump")
-        {
-            return m_jump;
-        }
-        if (key == "leftshift")
-        {
-            return m_leftshift;
-        }
-        if (key == "pause")
-        {
-            return m_Q;
-        }
-    }
+    float GetAxis(std::string axis);
+    glm::vec2 GetMousePosition() { return m_mousePosition; }
+    bool GetKey(std::string key);
 
 private:
-    InputManager() { 
-        m_horizontal = 0; 
-        m_vertical = 0; 
-        m_jump = false;
-        m_leftshift = false;
-        m_Q = false;
-        m_mousePosition = glm::vec2(0, 0);
-    }
-
+    InputManager();
 	InputManager(InputManager const&) = delete;
 	void operator= (InputManager const&) = delete;
 
